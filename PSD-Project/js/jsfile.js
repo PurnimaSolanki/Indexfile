427a09f1b64ef205d91d750ac43f86689faf3014
PurnimaSolanki <purnimasolanki2001@gmail.com>
2025-05-22 22:40:49 +0530

    PSD

---
+++ PSD-Project/js/jsfile.js
@@ -0,0 +1,10 @@
+nav = document.querySelector(".navbar")
+// bar = document.querySelector(".bar")
+function cl(){
+    if( nav.style.display == "none"){
+        nav.style.display="block"
+    }
+    else{
+        nav.style.display="none"
+    }
+     }
